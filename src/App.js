badaea14ad55c4949e81f3e4dad4bdc61a1ddaa6
MarshmallowJava <48638883+MarshmallowJava@users.noreply.github.com>
2025-09-17 20:28:56 +0900

    割合計算を実装

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  //候補
+  const [items, setItems] = useState([]);
+  const addItem = () => {
+    setItems([...items, {
+      amount: "0",
+      price: "0",
+    }]);
+  };
+  const updateItem = (index, field, value) => {
+    const newItems = [...items];
+    newItems[index][field] = value;
+    setItems(newItems);
+  };
+
+
+  //構築
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="title">
+        <h1>価格(計算).com</h1>
+      </div>
+      <div>
+        {
+          items.map((item, index) => (
+            <div className='item-block'>
+              <div key={index} className="item-row">
+                <Form.Control
+                  type="number"
+                  placeholder="量"
+                  value={item.amount}
+                  onChange={
+                    e => updateItem(index, "amount", e.target.value)
+                  }
+                  min="0"
+                />
+                <span>[ g | ml ] で</span>
+                <Form.Control
+                  type = "number"
+                  placeholder="価格"
+                  value={item.price}
+                  onChange={
+                    e => updateItem(index, "price", e.target.value)
+                  }
+                  min="0"
+                />
+                <span>円の商品</span>
+              </div>
+              <div>
+                <div>
+                  1[ g | ml ]当たり{item.amount > 0 ? item.price / item.amount : "NaN"} 円
+                </div>
+                <div>
+                  1円当たり{item.price > 0 ? item.amount / item.price : "NaN"} [ g | ml ]
+                </div>
+              </div>
+            </div>
+          ))
+        }
+        <Button onClick={addItem}>
+          New Entry
+        </Button>
+      </div>
+    </Container>
   );
 }
 
